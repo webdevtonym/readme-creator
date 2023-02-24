f3f9bd97cef08478ed5d9b25e8543ee04a741908
webdevtonym <75320313+webdevtonym@users.noreply.github.com>
2023-02-24 22:24:02 +0000

    added all objects and answers read me

--- index.js
+++ index.js
@@ -5,13 +5,52 @@ const fs = require('fs');
 inquirer.prompt([
     {
         type: 'input',
-        name: 'title',
+        name: 'Title',
         message: 'What is your project title?'
     },
     {
-        type: "",
-        name:
+        type: "input",
+        name: "Description",
+        message: "Please provide a description of the project?"
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Please provide installation instructions:"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Please provide usage information for the project:"
+
+    },
+    {
+        type: "list",
+        name: "Licence",
+        message: "Please choose a licence for the project:",
+        choices: ['MIT', 'GPL', 'cc', 'ms-pl']
+    },
+    {
+        type: 'input',
+        name: "Contributing",
+        message: "Please provide contribution guidelines for the project:"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Please provide test instructions for the project:"
+    },
+    {
+        type: "input",
+        name: "Github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
     }
+
 ])
 .then((answers) => {
     const readme = `${answers.title}\n\n Read me for the project`;
